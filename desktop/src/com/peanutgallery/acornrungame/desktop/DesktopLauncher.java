1c47df2722795fcc502743c87edffcfa4a8896f8
unknown <yhu@gloran.ctf.com>
2014-05-17 15:35:27 -0700

    desktop size and title

--- desktop/src/com/peanutgallery/acornrungame/desktop/DesktopLauncher.java
+++ desktop/src/com/peanutgallery/acornrungame/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.peanutgallery.acornrungame.AcornRunGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "AcornRun";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new AcornRunGame(), config);
 	}
 }
