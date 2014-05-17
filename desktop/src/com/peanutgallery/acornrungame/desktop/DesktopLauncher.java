5f5d9c90d8190ba0eb66461e4267a0608dc12fe9
amelia-ylh <amelia_ylh@hotmail.com>
2014-05-17 13:51:54 -0700

    initial commit

---
+++ desktop/src/com/peanutgallery/acornrungame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.peanutgallery.acornrungame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.peanutgallery.acornrungame.AcornRunGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AcornRunGame(), config);
+	}
+}
