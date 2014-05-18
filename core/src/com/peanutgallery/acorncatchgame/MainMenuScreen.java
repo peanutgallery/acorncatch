426fd2bcfdc04736105dadcf317bd97f06125cb4
amelia-ylh <amelia_ylh@hotmail.com>
2014-05-17 22:04:11 -0700

    changed text on MainMenuScreen

--- core/src/com/peanutgallery/acorncatchgame/MainMenuScreen.java
+++ core/src/com/peanutgallery/acorncatchgame/MainMenuScreen.java
@@ -28,7 +28,7 @@ public class MainMenuScreen implements Screen {
         game.batch.setProjectionMatrix(camera.combined);
 
         game.batch.begin();
-        game.font.draw(game.batch, "Welcome to AcornDrop!!! ", 100, 150);
+        game.font.draw(game.batch, "Welcome to AcornCatch!!! ", 100, 150);
         game.font.draw(game.batch, "Tap anywhere to begin.", 100, 100);
         game.batch.end();
 
